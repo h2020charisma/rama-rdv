8e4cc5ef2066faf3a42bfc0baa2e678091fe12aa
vedina <jeliazkova.nina@gmail.com>
2023-08-18 16:41:00 +0300

    H5Grove provider with uuid param

--- src/vite_rdv/src/MyApp.tsx
+++ src/vite_rdv/src/MyApp.tsx
@@ -1,13 +1,26 @@
-import { App, MockProvider } from '@h5web/app';
+import React from 'react';
+import { App,  H5GroveProvider } from '@h5web/app';
 import '@h5web/app/dist/styles.css';
 
+const URL = 'https://api.ramanchada.ideaconsult.net/h5grove';
+const FILE_DEFAULT = 'e7e9918a-85ea-4551-a31c-b40327aba8af.nxs';
+
+function getQueryParam(name : string) {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  return urlSearchParams.get(name);
+}
+
 function MyApp() {
-
-
+  const uuid = getQueryParam('uuid');
+  const FILEPATH = uuid ? `${uuid}.nxs` : FILE_DEFAULT;
   return (
-    <MockProvider>
-      <App  />
-    </MockProvider>
+    <H5GroveProvider
+      url={URL}
+      filepath={FILEPATH}
+      axiosConfig={{ params: { file: FILEPATH } }}
+    >
+      <App />
+    </H5GroveProvider>
   );
 
 }
