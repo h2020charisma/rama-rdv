91970880e4b32b01df55bbe5e7e338019262bcf6
vedina <jeliazkova.nina@gmail.com>
2023-08-19 11:13:51 +0300

    with css

--- src/vite_upload/wizard_convert/src/TWizardUploadApp.tsx
+++ src/vite_upload/wizard_convert/src/TWizardUploadApp.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import './TWizardUploadApp.css';
 interface SpectraFormData {
   provider: string;
   instrument: string;
@@ -65,16 +65,17 @@ function TWizardUploadApp() {
   };
 
   return (
-    <div>
-      <h2>File Uploader</h2>
+    <div className="container mt-5">
+      <h2 className="mb-4">File Uploader</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>
+        <div className="mb-3">
+          <label className="me-3">
             <input
               type="radio"
               value="spectra"
               checked={uploadType === 'spectra'}
               onChange={handleUploadTypeChange}
+              className="me-1"
             />
             Spectra Upload
           </label>
@@ -84,6 +85,7 @@ function TWizardUploadApp() {
               value="excel"
               checked={uploadType === 'excel'}
               onChange={handleUploadTypeChange}
+              className="me-1"
             />
             Excel Upload
           </label>
@@ -91,87 +93,120 @@ function TWizardUploadApp() {
         {uploadType === 'spectra' && (
           <div>
             {/* Add other input fields for spectra form */}
-            <input
-              type="file"
-              accept=".txt, .csv"
-              onChange={handleSpectraFileChange}
-            />
-            <input
-              type="text"
-              name="investigation"
-              value={spectraFormData.investigation}
-              onChange={(event) =>
-                setSpectraFormData({ ...spectraFormData, investigation: event.target.value })
-              }
-              placeholder="Investigation"
-            />
-            <input
-              type="text"
-              name="provider"
-              value={spectraFormData.provider}
-              onChange={(event) =>
-                setSpectraFormData({ ...spectraFormData, provider: event.target.value })
-              }
-              placeholder="Provider"
-            />
-            <input
-              type="text"
-              name="instrument"
-              value={spectraFormData.instrument}
-              onChange={(event) =>
-                setSpectraFormData({ ...spectraFormData, instrument: event.target.value })
-              }
-              placeholder="Instrument"
-            />  
-            <input
-              type="text"
-              name="wavelength"
-              value={spectraFormData.wavelength}
-              onChange={(event) =>
-                setSpectraFormData({ ...spectraFormData, wavelength: event.target.value })
-              }
-              placeholder="wavelength"
-            />     
-            <input
-              type="text"
-              name="sample"
-              value={spectraFormData.sample}
-              onChange={(event) =>
-                setSpectraFormData({ ...spectraFormData, sample: event.target.value })
-              }
-              placeholder="sample"
-            />                                    
-            <input
-              type="text"
-              name="sample_provider"
-              value={spectraFormData.sample_provider}
-              onChange={(event) =>
-                setSpectraFormData({ ...spectraFormData, sample_provider: event.target.value })
-              }
-              placeholder="sample_provider"
-            /> 
-            <button type="submit">Upload Spectra</button>
+            <div className="row mb-3">
+                <input
+                  type="file"
+                  accept=".txt, .csv"
+                  onChange={handleSpectraFileChange}
+                />
+            </div>
+            <div className="row mb-3">
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="investigation"
+                  value={spectraFormData.investigation}
+                  onChange={(event) =>
+                    setSpectraFormData({ ...spectraFormData, investigation: event.target.value })
+                  }
+                  placeholder="Investigation"
+                />
+              </div>
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="provider"
+                  value={spectraFormData.provider}
+                  onChange={(event) =>
+                    setSpectraFormData({ ...spectraFormData, provider: event.target.value })
+                  }
+                  placeholder="Provider"
+                />
+              </div>              
+            </div>
+            <div className="row mb-3">
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="instrument"
+                  value={spectraFormData.instrument}
+                  onChange={(event) =>
+                    setSpectraFormData({ ...spectraFormData, instrument: event.target.value })
+                  }
+                  placeholder="instrument"
+                />
+              </div>
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="wavelength"
+                  value={spectraFormData.wavelength}
+                  onChange={(event) =>
+                    setSpectraFormData({ ...spectraFormData, wavelength: event.target.value })
+                  }
+                  placeholder="wavelength"
+                />
+              </div>              
+            </div>
+            <div className="row">
+            <div className="col-md-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="wavelength"
+                  value={spectraFormData.sample}
+                  onChange={(event) =>
+                    setSpectraFormData({ ...spectraFormData, sample: event.target.value })
+                  }
+                  placeholder="sample"
+                />
+              </div>  
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="sample_provider"
+                  value={spectraFormData.sample_provider}
+                  onChange={(event) =>
+                    setSpectraFormData({ ...spectraFormData, sample_provider: event.target.value })
+                  }
+                  placeholder="sample_provider"
+                />
+              </div>                
+            </div>
+            <button type="submit" className="btn btn-primary">Upload Spectra</button>
           </div>
         )}
         {uploadType === 'excel' && (
           <div>
-            {/* Add input fields for excel form */}
-            <input
-              type="file"
-              accept=".xlsx"
-              onChange={handleExcelFileChange}
-            />
-            <input
-              type="file"
-              accept=".json"
-              onChange={handleJsonFileChange}
-            />
-            <button type="submit">Upload Excel</button>
+            <div className="row mb-3">
+              {/* Add input fields for excel form */}
+              <div className="col-md-6">
+                <input
+                  type="file"
+                  accept=".xlsx"
+                  onChange={handleExcelFileChange}
+                />
+              </div>
+              <div className="col-md-6">
+                <input
+                  type="file"
+                  accept=".json"
+                  onChange={handleJsonFileChange}
+                />
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary">Upload Excel</button>
           </div>
         )}
       </form>
     </div>
   );
+  
 }
 
 export default TWizardUploadApp;
